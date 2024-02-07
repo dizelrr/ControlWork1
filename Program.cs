6470404f472f059a86adce2b7170d71a770b48fe
dizelrr <dizel-rr@mail.ru>
2024-02-07 20:45:44 +0300

    2 продолжение

--- Program.cs
+++ Program.cs
@@ -9,6 +9,48 @@ string[] FillArray()
     string? enterSymbols = Console.ReadLine();
     if (enterSymbols == null) { enterSymbols = ""; };
     char[] separators = new char[] { ',', ' ' };
-    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-    return workArray;
+    string[] createdArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return createdArray;
+}
+string PrintArray(string[] createdArray)
+{
+    string stringArray = "[";
+    for (int i = 0; i < createdArray.Length; i++)
+    {
+        if (i == createdArray.Length - 1)
+        {
+            stringArray += $"\"{createdArray[i]}\"";
+            break;
+        }
+        stringArray += ($"\"{createdArray[i]}\", ");
+    }
+    stringArray += "]";
+    return stringArray;
+}
+int CountStringSymbols(string[] createdArray)
+{
+    int counter = 0;
+    foreach (string item in createdArray)
+    {
+        if (item.Length <= 3)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+string[] GenerateNewArray(string[] createdArray)
+{
+    int resultArrayLength = CountStringSymbols(createdArray);
+    string[] resultArray = new string[resultArrayLength];
+    int i = 0;
+    foreach (string item in createdArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[i] = item;
+            i++;
+        }
+    }
+    return resultArray;
 }
