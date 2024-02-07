4b54abf27940f75098492f33dd122bbf4c713a1b
dizelrr <dizel-rr@mail.ru>
2024-02-07 20:12:44 +0300

    Начало

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArray;
+}
